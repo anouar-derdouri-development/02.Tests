cb7ac77de4d7dfbade0b0be5e8a61191436171c0
Anouar DERDOURI <118266778+anouar-derdouri-development@users.noreply.github.com>
2023-01-07 12:37:51 +0000

    Add custom setters

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,29 +1,40 @@
 class Person constructor(
-    var firstName: String,
-    var lastName: String,
+    firstName: String,
+    lastName: String,
     /*
+        ? firstName & lastname are juste properties because we want to add their custom getters & setters, but we first have to add properties inside class body
+
         * ToDo: Add private property
 
         * ToDo: Add val property
      */
 ) {
-    var fullName: String = "$firstName $lastName"
-        /*
-            * We can initialize fullName in init block
-        */
+    var firstName = firstName
+        set(value) {
+            field = value.substring(0, 1).uppercase() + value.substring(1).lowercase()
+        }
+
+    var lastName = lastName
+        set(value) {
+            field = value.uppercase()
+        }
+
+    val fullName: String // ? no need to initialize fullName because it has a getter + it has a private setter→ Convert fullName to val
         get() {
             return "$firstName $lastName"
         }
-        private set
 
     init {
         println("\ninit")
         println("-".repeat("init".length))
 
-        firstName = firstName.substring(0, 1).uppercase() + firstName.substring(1).lowercase()
-        lastName = lastName.uppercase()
+        this.firstName = this.firstName.substring(0, 1).uppercase() + this.firstName.substring(1).lowercase()
+        this.lastName = this.lastName.uppercase()
+        /*
+            ? The previous 2 lines are required (even the existence of the setters) because the primary constructor doesn't meet the setters but th init block is always executed after primary constructor call
+        */
 
-        println("$firstName $lastName")
+        println("${this.firstName} ${this.lastName}")
     }
 
     fun show() {
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -2,23 +2,10 @@ fun main() {
     var p = Person("hassan", "alaoui")
 
     p.show()
-    println("Fullname: ${p.fullName}")// * Call the custom getter
-//    p.fullName = ""
-    // ? Doesn't work because the setter is private
+    println("Fullname: ${p.fullName}")
 
     p.firstName = "hamid"
     p.lastName = "hassani"
     p.show()
-    /*
-        ? "hamid" & "hassani" will stay "hamid" & "hassani"
-        ? not "Hamid" & "HASSANI" (like init block)
-
-        ToDo: Add custom setters or getters
-    */
-
     println("Fullname: ${p.fullName}")
-    /*
-        ? "hamid hassani"
-        ? not "Hamid HASSANI"
-    */
 }
